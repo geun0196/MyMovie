1bb5322a9a6c33cd300d4af416c29123b915d008
geun0196 <geun0196@naver.com>
2023-02-01 23:44:42 +0900

    Initial Commit

---
+++ iMovie/iMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMovieApp.swift
+//  iMovie
+//
+//  Created by PC on 2023/02/01.
+//
+
+import SwiftUI
+
+@main
+struct iMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
