a3fc29801dbb3b0b0dfdcd0ce85853f009b807b5
geun0196 <geun0196@naver.com>
2023-02-13 21:08:40 +0900

    first commit

---
+++ iMovie/ActorView.swift
@@ -0,0 +1,20 @@
+//
+//  ActorView.swift
+//  iMovie
+//
+//  Created by PC on 2023/02/13.
+//
+
+import SwiftUI
+
+struct ActorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ActorView_Previews: PreviewProvider {
+    static var previews: some View {
+        ActorView()
+    }
+}
---
+++ iMovie/MovieView.swift
@@ -0,0 +1,20 @@
+//
+//  MovieView.swift
+//  iMovie
+//
+//  Created by PC on 2023/02/13.
+//
+
+import SwiftUI
+
+struct MovieView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MovieView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieView()
+    }
+}
